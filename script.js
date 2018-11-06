fbe7cf3a98f142dca1ee464c53e32550dc93641b
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-11-06 19:01:55 +0100

    Expand app - posters + movie records

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 var movies = [
     {
-      id: 1, // for optimalisation
-      title: 'Harry Potter',
-      desc: 'film o czarodzieju'
+        id: 1, // for optimalisation
+        title: 'Harry Potter',
+        desc: 'film o czarodzieju',
+        poster: 'potter.jpg'
     },
     {
-      id: 2, // for optimalisation
-      title: 'Król Lew',
-      desc: 'Film o królu sawanny'
+        id: 2, // for optimalisation
+        title: 'Król Lew',
+        desc: 'film o królu sawanny',
+        poster: 'lion.jpg'
+    },
+    {
+        id: 3, // for optimalisation
+        title: 'John Wick',
+        desc: 'zabili mu pieska, więc zabił 77 osób',
+        poster: 'wick.jpg'
+    },
+    {
+        id: 4, // for optimalisation
+        title: 'Gwiezdne Wojny',
+        desc: 'ja sem twój tatinek',
+        poster: 'star.jpg'
+    },
+    {
+        id: 5, // for optimalisation
+        title: 'Władca Pierścieni',
+        desc: 'czemu nie wysłali orłów?!',
+        poster: 'ring.jpg'
+    },
+    {
+        id: 6, // for optimalisation
+        title: 'Dzień Świra',
+        desc: 'jak tatuś zrobi dzióbek... ;-)',
+        poster: 'swir.jpg'
     }
   ];
 
   var moviesElements = movies.map(function(movie) {
     return React.createElement('li', {key: movie.id}, // for optimalisation
         React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc)
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {className: 'logo', src: movie.poster})
       );
   });
 
