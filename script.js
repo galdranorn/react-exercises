0b0ddec89b7c875d2922021f6baf50077a8a38c9
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-11-07 19:56:07 +0100

    Change structure of the code; problem with more than one object rendering

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var movies = [
+/*const movies = [
     {
         id: 1, // for optimalisation
         title: 'Harry Potter',
@@ -9,25 +9,25 @@ var movies = [
         id: 2, // for optimalisation
         title: 'Król Lew',
         desc: 'film o królu sawanny',
-        poster: 'lion.jpg'
+        poster: 'potter.jpg'
     },
     {
         id: 3, // for optimalisation
         title: 'John Wick',
         desc: 'zabili mu pieska, więc zabił 77 osób',
-        poster: 'wick.jpg'
+        poster: 'potter.jpg'
     },
     {
         id: 4, // for optimalisation
         title: 'Gwiezdne Wojny',
         desc: 'ja sem twój tatinek',
-        poster: 'star.jpg'
+        poster: 'potter.jpg'
     },
     {
         id: 5, // for optimalisation
         title: 'Władca Pierścieni',
         desc: 'czemu nie wysłali orłów?!',
-        poster: 'ring.jpg'
+        poster: 'potter.jpg'
     },
     {
         id: 6, // for optimalisation
@@ -35,20 +35,35 @@ var movies = [
         desc: 'jak tatuś zrobi dzióbek... ;-)',
         poster: 'swir.jpg'
     }
-  ];
+  ];*/
 
-  var moviesElements = movies.map(function(movie) {
-    return React.createElement('li', {key: movie.id}, // for optimalisation
-        React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc),
-        React.createElement('img', {className: 'logo', src: movie.poster})
-      );
-  });
 
-  var element =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {}, moviesElements)
-  );
-  
+// --- CLASS MOVIE 
+let Movie = React.createClass ({
+    // render method
+    render: function() {
+      return (
+        React.createElement('li', {},
+            React.createElement('h2', {}, this.props.movie.title),
+            React.createElement('p', {}, this.props.movie.desc),
+            React.createElement('img', {className: 'logo', src: this.props.movie.poster})
+        ));  
+    
+    // validate component props
+    propTypes: {movie: React.PropTypes.object.isRequired}
+
+}});
+
+// data object
+const movie = {
+    id: 1,
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju',
+    poster: 'potter.jpg'
+}
+
+// creating element from class and data
+const element = React.createElement(Movie, {key: movie.id, movie: movie});
+
+// render to DOM
 ReactDOM.render(element, document.getElementById('app'));
