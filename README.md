bb21168ef6ebae163522f41bd99af21e082d66de
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-11-12 19:30:49 +0100

    Initial for exercise 4

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # react-exercises
 ReactJS exercises
+
+Each exercise in other branch.
+
+Exercise 1 - basic app creating list of movies from array of objects
+
+Exercise 2 - split code from exercise 1 to components, validate props
+
+Exercise 3 - creating contacts and form
+
+Exercise 4 - independent increment/decrement counters
+
+Exercise 5 - rewrite components from exercise 3 in JSX
--- script.js
+++ script.js
@@ -1,54 +1,16 @@
-var movies = [
-    {
-        id: 1, // for optimalisation
-        title: 'Harry Potter',
-        desc: 'film o czarodzieju',
-        poster: 'potter.jpg'
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
     },
-    {
-        id: 2, // for optimalisation
-        title: 'Król Lew',
-        desc: 'film o królu sawanny',
-        poster: 'lion.jpg'
-    },
-    {
-        id: 3, // for optimalisation
-        title: 'John Wick',
-        desc: 'zabili mu pieska, więc zabił 77 osób',
-        poster: 'wick.jpg'
-    },
-    {
-        id: 4, // for optimalisation
-        title: 'Gwiezdne Wojny',
-        desc: 'ja sem twój tatinek',
-        poster: 'star.jpg'
-    },
-    {
-        id: 5, // for optimalisation
-        title: 'Władca Pierścieni',
-        desc: 'czemu nie wysłali orłów?!',
-        poster: 'ring.jpg'
-    },
-    {
-        id: 6, // for optimalisation
-        title: 'Dzień Świra',
-        desc: 'jak tatuś zrobi dzióbek... ;-)',
-        poster: 'swir.jpg'
+
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        );
     }
-  ];
+});
 
-  var moviesElements = movies.map(function(movie) {
-    return React.createElement('li', {key: movie.id}, // for optimalisation
-        React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc),
-        React.createElement('img', {className: 'logo', src: movie.poster})
-      );
-  });
-
-  var element =
-  React.createElement('div', {},
-    React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {}, moviesElements)
-  );
-  
+var element = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById('app'));
