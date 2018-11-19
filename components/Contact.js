53fac0d74dc1b9aee48eec52cd5b33fa9eb2203b
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-11-19 16:39:49 +0100

    Change babel ver. and rewrite the JSX parts

--- components/Contact.js
+++ components/Contact.js
@@ -6,6 +6,7 @@ var Contact = React.createClass({
     render: function() {
       return (
         <div className={'contactItem col-lg-6 col-md-6 col-sm-12'}>
+<<<<<<< Updated upstream
             <div className={'contactImg col-lg-4 col-md-4 col-sm-'}>
                 <img className={'contactImage'} src={'http://icons.veryicon.com/png/System/gCons/contacts.png'}></img>
             </div>
@@ -16,5 +17,17 @@ var Contact = React.createClass({
             </div>
         </div>
       )
+=======
+            <div className={'contactImg col-lg-4 col-md-4 col-sm-4'}>
+                <img className={'contactImage'} src={'http://icons.veryicon.com/png/System/gCons/contacts.png'}/>
+            </div>
+            <div className={'contactItem col-lg-6 col-md-6 col-sm-12'}>
+                <p className={'contactLabel'}>Imię: ${this.props.item.firstName}</p>
+                <p className={'contactLabel'}>Nazwisko: ${this.props.item.lastName}</p>
+                <a className={'contactEmail'} href={'mailto:'+this.props.item.email}>${this.props.item.email}</a>
+            </div>
+        </div>
+        )
+>>>>>>> Stashed changes
     },
   });
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,11 +6,20 @@ var ContactForm = React.createClass({
     render: function() {
       return (
         <form className={'contactForm'}>
+<<<<<<< Updated upstream
           <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}></input>
           <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}></input>
           <input type={'text'} placeholder={'Email'} value={this.props.contact.email}></input>
           <button type={'submit'}>Dodaj kontakt</button>
         </form>
       )
+=======
+          <input type={'text'} placeholder={'Imię'} value={this.props.contact.firstName}/>
+          <input type={'text'} placeholder={'Nazwisko'} value={this.props.contact.lastName}/>
+          <input type={'email'} placeholder={'E-mail'} value={this.props.contact.email}/>
+          <button type={'submit'}>Dodaj kontakt</button>
+        </form>
+        )
+>>>>>>> Stashed changes
     },
   })
